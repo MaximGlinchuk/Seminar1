33b670e401f550692dfc5d045ddafa8fb337cbce
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-13 17:48:33 +0400

    complete task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count <= number)
+{
+    if (count % 2 ==0)
+    {
+        Console.WriteLine("Четные числа: " + count);
+    }
+    count++;
+}
