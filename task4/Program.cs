eaf948832cdf01bdaa3b711c9bc406cdc3ca5703
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-13 17:45:49 +0400

    complete task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > number1)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine("Максимальное чило: " + max);
